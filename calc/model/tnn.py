2d1f8ba70862a301fe2d873f024d3c09cd8fe906
zhangyikuo <yakolle@aliyun.com>
2018-08-12 21:03:06 +0800

    tnn

--- calc/model/tnn.py
+++ calc/model/tnn.py
@@ -23,9 +23,27 @@ def get_default_mse_output(flat, oh_input=None, cat_input=None, seg_input=None, 
     return dnn
 
 
+def get_default_bce_output(flat, oh_input=None, cat_input=None, seg_input=None, num_input=None):
+    flat = add_dense(flat, 64, bn=False, dropout=0.05)
+    output = Dense(1, activation='sigmoid')(flat)
+
+    inputs = [oh_input] if oh_input is not None else []
+    if cat_input is not None:
+        inputs.append(cat_input)
+    if seg_input is not None:
+        inputs.append(seg_input)
+    if num_input is not None:
+        inputs.append(num_input)
+
+    dnn = Model(inputs, output)
+    dnn.compile(loss='binary_crossentropy', optimizer=Adam(lr=1e-3), metrics=['acc'])
+    return dnn
+
+
 def get_tnn_model(x, get_output=get_default_mse_output, cat_in_dims=None, cat_out_dims=None, seg_out_dims=None,
                   num_segs=None, use_fm=False, seg_flag=True, add_seg_src=True, seg_num_flag=True,
-                  get_extra_layers=None, fm_dim=320, fm_dropout=0.2, hidden_units=320, hidden_dropout=0.2):
+                  get_extra_layers=None, fm_dim=320, fm_dropout=0.2, fm_activation=None, hidden_units=320,
+                  hidden_dropout=0.2):
     oh_input = Input(shape=[x['ohs'].shape[1]], name='ohs') if 'ohs' in x else None
     cat_input = Input(shape=[x['cats'].shape[1]], name='cats') if 'cats' in x else None
     seg_input = Input(shape=[x['segs'].shape[1]], name='segs') if 'segs' in x else None
@@ -52,7 +70,7 @@ def get_tnn_model(x, get_output=get_default_mse_output, cat_in_dims=None, cat_ou
     extra_feats = get_extra_layers(x, feats) if get_extra_layers is not None else None
 
     if use_fm:
-        fm = FMLayer(fm_dim, activation=None)(feats)
+        fm = FMLayer(fm_dim, activation=fm_activation)(feats)
         fm = Dropout(fm_dropout)(fm)
         flat = concatenate([feats, fm])
     else:

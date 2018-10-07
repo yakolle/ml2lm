3a760b445d88f6bf5cfbdb037bdd04d405bf792a
yakolle <yakolle@aliyun.com>
2018-10-07 09:39:54 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='ml2lm',
+      version='0.1',
+      description='ml2lm',
+      url='http://github.com/yakolle/ml2lm',
+      author='Yakolle Zhang',
+      license='MIT',
+      packages=['calc'],
+      zip_safe=False)

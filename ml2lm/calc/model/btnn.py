4788d877c24aa84c19b4c2394677b5665720c148
zhangyikuo <yakolle@aliyun.com>
2019-05-07 18:57:18 +0800

    update fm config

--- ml2lm/calc/model/btnn.py
+++ ml2lm/calc/model/btnn.py
@@ -5,24 +5,28 @@ def get_btnn_model(x, y, get_output=get_linear_output, compile_func=compile_defa
                    cat_out_dims=None, seg_out_dims=None, num_segs=None, seg_type=0, seg_x_val_range=(0, 1), block_num=3,
                    shrink_factor=1.0, seg_y_dim=50, prev_block_weight_files=None, use_fm=False, seg_flag=True,
                    add_seg_src=True, seg_num_flag=True, get_extra_layers=None, embed_dropout=0.2, seg_func=seu,
-                   seg_dropout=0.2, fm_dim=320, fm_dropout=0.2, fm_activation=None,
-                   get_last_layers=get_default_dense_layers, hidden_units=(320, 64), hidden_activation=seu,
-                   hidden_dropouts=(0.2, 0.05)):
+                   seg_dropout=0.1, fm_dim=320, fm_dropout=0.3, fm_activation='relu', fm_dist_func=lrelu,
+                   fm_rel_types='d', fm_exclude_self=False, get_last_layers=get_default_dense_layers,
+                   hidden_units=(320, 64), hidden_activation=seu, hidden_dropouts=(0.3, 0.05)):
     cat_input = Input(shape=[x['cats'].shape[1]], name='cats') if 'cats' in x else None
     seg_input = Input(shape=[x['segs'].shape[1]], name='segs') if 'segs' in x else None
     num_input = Input(shape=[x['nums'].shape[1]], name='nums') if 'nums' in x else None
     extra_inputs = None
     seg_y_val_range = (np.min(y), np.max(y))
 
+    params = {'get_output': get_output, 'cat_input': cat_input, 'seg_input': seg_input, 'num_input': num_input,
+              'cat_in_dims': cat_in_dims, 'cat_out_dims': cat_out_dims, 'seg_out_dims': seg_out_dims,
+              'num_segs': num_segs, 'seg_type': seg_type, 'seg_x_val_range': seg_x_val_range,
+              'seg_y_val_range': seg_y_val_range, 'seg_y_dim': seg_y_dim, 'shrink_factor': shrink_factor,
+              'use_fm': use_fm, 'seg_flag': seg_flag, 'add_seg_src': add_seg_src, 'seg_num_flag': seg_num_flag, 'x': x,
+              'get_extra_layers': get_extra_layers, 'embed_dropout': embed_dropout, 'seg_func': seg_func,
+              'seg_dropout': seg_dropout, 'fm_dim': fm_dim, 'fm_dropout': fm_dropout, 'fm_activation': fm_activation,
+              'fm_dist_func': fm_dist_func, 'fm_rel_types': fm_rel_types, 'fm_exclude_self': fm_exclude_self,
+              'get_last_layers': get_last_layers, 'hidden_units': hidden_units, 'hidden_activation': hidden_activation,
+              'hidden_dropouts': hidden_dropouts}
+
     if block_num <= 1:
-        btnn, extra_inputs = get_tnn_block(
-            0, get_output=get_output, cat_input=cat_input, seg_input=seg_input, num_input=num_input,
-            cat_in_dims=cat_in_dims, cat_out_dims=cat_out_dims, seg_out_dims=seg_out_dims, num_segs=num_segs,
-            seg_type=seg_type, seg_x_val_range=seg_x_val_range, use_fm=use_fm, seg_flag=seg_flag,
-            add_seg_src=add_seg_src, seg_num_flag=seg_num_flag, x=x, get_extra_layers=get_extra_layers,
-            embed_dropout=embed_dropout, seg_func=seg_func, seg_dropout=seg_dropout, fm_dim=fm_dim,
-            fm_dropout=fm_dropout, fm_activation=fm_activation, get_last_layers=get_last_layers,
-            hidden_units=hidden_units, hidden_activation=hidden_activation, hidden_dropouts=hidden_dropouts)
+        btnn, extra_inputs = get_tnn_block(0, **params)
         btnn = compile_func(btnn, cat_input=cat_input, seg_input=seg_input, num_input=num_input,
                             other_inputs=extra_inputs)
     else:
@@ -30,16 +34,9 @@ def get_btnn_model(x, y, get_output=get_linear_output, compile_func=compile_defa
             outputs = []
             prev_output = None
             for i in range(block_num):
-                cur_output, extra_inputs = get_tnn_block(
-                    i, get_output=get_output, cat_input=cat_input, seg_input=seg_input, num_input=num_input,
-                    pre_output=prev_output, cat_in_dims=cat_in_dims, cat_out_dims=cat_out_dims,
-                    seg_out_dims=seg_out_dims, num_segs=num_segs, seg_type=seg_type, seg_x_val_range=seg_x_val_range,
-                    seg_y_val_range=seg_y_val_range, seg_y_dim=seg_y_dim, shrink_factor=shrink_factor, use_fm=use_fm,
-                    seg_flag=seg_flag, add_seg_src=add_seg_src, seg_num_flag=seg_num_flag, x=x,
-                    extra_inputs=extra_inputs, get_extra_layers=get_extra_layers, embed_dropout=embed_dropout,
-                    seg_func=seg_func, seg_dropout=seg_dropout, fm_dim=fm_dim, fm_dropout=fm_dropout,
-                    fm_activation=fm_activation, get_last_layers=get_last_layers, hidden_units=hidden_units,
-                    hidden_activation=hidden_activation, hidden_dropouts=hidden_dropouts)
+                params['pre_output'] = prev_output
+                params['extra_inputs'] = extra_inputs
+                cur_output, extra_inputs = get_tnn_block(i, **params)
                 outputs.append(cur_output)
                 prev_output = cur_output
             btnn = compile_func(outputs, cat_input=cat_input, seg_input=seg_input, num_input=num_input,
@@ -49,7 +46,8 @@ def get_btnn_model(x, y, get_output=get_linear_output, compile_func=compile_defa
                 x, y, get_output, compile_func, cat_in_dims, cat_out_dims, seg_out_dims, num_segs, seg_type,
                 seg_x_val_range, block_num - 1, shrink_factor, seg_y_dim, prev_block_weight_files, use_fm, seg_flag,
                 add_seg_src, seg_num_flag, get_extra_layers, embed_dropout, seg_func, seg_dropout, fm_dim, fm_dropout,
-                fm_activation, get_last_layers, hidden_units, hidden_activation, hidden_dropouts)
+                fm_activation, fm_dist_func, fm_rel_types, fm_exclude_self, get_last_layers, hidden_units,
+                hidden_activation, hidden_dropouts)
             read_weights(btnn, prev_block_weight_files[block_num - 2])
             for layer in btnn.layers:
                 layer.trainable = False
@@ -68,16 +66,13 @@ def get_btnn_model(x, y, get_output=get_linear_output, compile_func=compile_defa
 
             outputs = btnn.outputs
             prev_output = outputs[-1]
-            cur_output, extra_inputs = get_tnn_block(
-                block_num - 1, get_output=get_output, cat_input=cat_input, seg_input=seg_input, num_input=num_input,
-                pre_output=prev_output, cat_in_dims=cat_in_dims, cat_out_dims=cat_out_dims, seg_out_dims=seg_out_dims,
-                num_segs=num_segs, seg_type=seg_type, seg_x_val_range=seg_x_val_range, seg_y_val_range=seg_y_val_range,
-                seg_y_dim=seg_y_dim, shrink_factor=shrink_factor, use_fm=use_fm, seg_flag=seg_flag,
-                add_seg_src=add_seg_src, seg_num_flag=seg_num_flag, x=x, extra_inputs=extra_inputs,
-                get_extra_layers=get_extra_layers, embed_dropout=embed_dropout, seg_func=seg_func,
-                seg_dropout=seg_dropout, fm_dim=fm_dim, fm_dropout=fm_dropout, fm_activation=fm_activation,
-                get_last_layers=get_last_layers, hidden_units=hidden_units, hidden_activation=hidden_activation,
-                hidden_dropouts=hidden_dropouts)
+
+            params['cat_input'] = cat_input
+            params['seg_input'] = seg_input
+            params['num_input'] = num_input
+            params['extra_inputs'] = extra_inputs
+            params['pre_output'] = prev_output
+            cur_output, extra_inputs = get_tnn_block(block_num - 1, **params)
             outputs.append(cur_output)
 
             btnn = compile_func(outputs, cat_input=cat_input, seg_input=seg_input, num_input=num_input,
